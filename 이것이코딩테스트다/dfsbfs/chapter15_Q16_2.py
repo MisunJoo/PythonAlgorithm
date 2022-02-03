6853458377b57b47f437d0cacefc65a130b1ffec
MisunJoo <0987someday@naver.com>
2022-02-03 20:58:13 +0900

    연구소

--- 이것이코딩테스트다/dfsbfs/chapter15_Q16_2.py
+++ 이것이코딩테스트다/dfsbfs/chapter15_Q16_2.py
@@ -13,8 +13,9 @@ dy = [0, 1, 0, -1]
 
 result = 0
 
-# 깊이 우선 탐색(DFS)를 이용해 각 바이러스가 사방으로 퍼지도록 하기
-def virus(x, y):
+###########
+
+def spreadVirus(x, y):
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
@@ -23,7 +24,7 @@ def virus(x, y):
             if temp[nx][ny] == 0:
                 # 해당 위치에 바이러스 배치하고, 다시 재귀적으로 수행
                 temp[nx][ny] = 2
-                virus(nx, ny)
+                spreadVirus(nx, ny)
 
 def get_score():
     score = 0
@@ -33,31 +34,31 @@ def get_score():
                 score += 1
     return score
 
-# 깊이 우선 탐색(DFS)를 이용해 울타리를 설치하면서, 매번 안전 영역의 크기 계산
-def dfs(count):
+def makeWall(cnt):
     global result
-    # 울타리가 3개 설치된 경우
-    if count == 3:
+    if cnt == 3:
+        # 복사
         for i in range(n):
             for j in range(m):
                 temp[i][j] = data[i][j]
-        # 각 바이러스의 위치에서 전파 진행
+        # 바이러스 퍼트리기
         for i in range(n):
             for j in range(m):
                 if temp[i][j] == 2:
-                    virus(i, j)
+                    spreadVirus(i, j) # 여기서 해당 울타리가 쳐졌을 때 바이러스를 전부 퍼트림
         # 안전 영역의 최댓값 계산
         result = max(result, get_score())
         return
-    # 빈 공간에 울타리 설치
+
+    # 울타리 설치
     for i in range(n):
         for j in range(m):
             if data[i][j] == 0:
                 data[i][j] = 1
-                count += 1
-                dfs(count)
+                cnt += 1
+                makeWall(cnt)
+                # 벽 만든 후 초기화
                 data[i][j] = 0
-                count -= 1
+                cnt -= 1
 
-dfs(0)
-print(result)
+makeWall(0)
